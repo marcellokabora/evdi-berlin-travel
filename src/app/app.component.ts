f60bfc4dd28a0029168135f9cb589655fddeae32
Marcello <marcellokabora@gmail.com>
2022-07-19 17:20:15 +0200

    berlin travel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
+import { Pipe, PipeTransform } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,101 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'evdi-berlin-travel';
+
+  tours: any = []
+
+  search: string = ''
+
+  ratings = this._formBuilder.group({
+    star1: false,
+    star2: false,
+    star3: false,
+    star4: false,
+    star5: false,
+  })
+  minrate = 1
+  maxrate = 5
+
+  pricing = this._formBuilder.group({
+    price1: false,
+    price2: false,
+    price3: false,
+    price4: false,
+    price5: false,
+  })
+  minprice = 0
+  maxprice = 1000
+
+  constructor(
+    private http: HttpClient,
+    private _formBuilder: FormBuilder
+  ) {
+    this.http.get("./assets/data.json").subscribe((data: any) => {
+      this.tours = data.tours
+    })
+
+
+    this.ratings.valueChanges.subscribe(values => {
+      const rating = []
+      if (values.star1) {
+        rating.push(1)
+      }
+      if (values.star2) {
+        rating.push(2)
+      }
+      if (values.star3) {
+        rating.push(3)
+      }
+      if (values.star4) {
+        rating.push(4)
+      }
+      if (values.star5) {
+        rating.push(5)
+      }
+      this.minrate = rating[0] ? rating[0] : 1
+      this.maxrate = rating[rating.length - 1] ? rating[rating.length - 1] + .9 : 5
+    })
+
+    this.pricing.valueChanges.subscribe(values => {
+      const pricing = []
+      if (values.price1) {
+        pricing.push(0, 10)
+      }
+      if (values.price2) {
+        pricing.push(10, 20)
+      }
+      if (values.price3) {
+        pricing.push(20, 50)
+      }
+      if (values.price4) {
+        pricing.push(50, 100)
+      }
+      if (values.price5) {
+        pricing.push(1000)
+      }
+      this.minprice = pricing[0] ? pricing[0] : 0
+      this.maxprice = pricing[pricing.length - 1] ? pricing[pricing.length - 1] : 1000
+    })
+
+  }
+
+}
+
+@Pipe({ name: 'rating' })
+export class pipeRating implements PipeTransform {
+  transform(items: any, min: number, max: number): any {
+    return items.filter((item: any) => item.rating >= min && item.rating <= max)
+  }
+}
+@Pipe({ name: 'pricing' })
+export class pipePricing implements PipeTransform {
+  transform(items: any, min: number, max: number): any {
+    return items.filter((item: any) => item.price >= min && item.price <= max)
+  }
+}
+@Pipe({ name: 'searching' })
+export class pipeSearching implements PipeTransform {
+  transform(items: any, search: string): any {
+    return items.filter((item: any) => item.title.toUpperCase().indexOf(search.toUpperCase()) > -1)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { pipeRating, pipePricing, pipeSearching } from './app.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    pipeRating,
+    pipePricing,
+    pipeSearching
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    HttpClientModule,
+    MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
